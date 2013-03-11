c3e5e1f1b35efee70d73d2143a8b5b5522753713
Raoni Normanton <raonifn@gmail.com>
2013-03-11 09:44:00 -0300

    Including Java property casperjs.executable to custom the casperjs location

---
+++ src/test/casperjs/dummy.test.js
@@ -0,0 +1,6 @@
+var casper = require('casper').create({
+	verbose : true,
+	logLevel : "debug",
+	colorizerType : 'Dummy'
+});
+casper.exit(0);
---
+++ src/test/java/com/github/raonifn/casperjs/junit/CasperExecutorTest.java
@@ -0,0 +1,38 @@
+package com.github.raonifn.casperjs.junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.net.URL;
+
+import org.junit.Test;
+
+public class CasperExecutorTest {
+
+	@Test
+	public void testCasperjsLocation() {
+		URL resource = Thread.currentThread().getContextClassLoader().getResource("dummy.test.js");
+		String file = resource.getPath();
+
+		CasperExecutor executor = createExecutor();
+		assertEquals(0, executor.executeCasper(file));
+
+		System.setProperty("casperjs.executable", "./foo.bar");
+		executor = createExecutor();
+		try {
+			executor.executeCasper(file);
+			fail("Must throw exception");
+		} catch (RuntimeException ex) {
+			assertTrue(ex.getMessage().contains("./foo.bar"));
+		}
+
+	}
+
+	private CasperExecutor createExecutor() {
+		CasperExecutor executor = new CasperExecutor();
+		executor.pipeOut(System.out);
+		executor.pipeErr(System.err);
+		return executor;
+	}
+}
