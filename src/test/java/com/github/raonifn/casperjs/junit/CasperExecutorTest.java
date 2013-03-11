a6b477a062f1d4074ae43e2b27433fb72f13546a
Raoni Normanton <raonifn@gmail.com>
2013-03-11 11:39:48 -0300

    Fixing

--- src/test/java/com/github/raonifn/casperjs/junit/CasperExecutorTest.java
+++ src/test/java/com/github/raonifn/casperjs/junit/CasperExecutorTest.java
@@ -25,6 +25,8 @@ public class CasperExecutorTest {
 			fail("Must throw exception");
 		} catch (RuntimeException ex) {
 			assertTrue(ex.getMessage().contains("./foo.bar"));
+		} finally {
+			System.setProperty("casperjs.executable", null);
 		}
 
 	}
